448f403e1112bb71b7963670c282020c20507075
Yeshua Avalos Trejo <yavalost@asu.edu>
2026-04-24 11:45:51 -0700

    Add files via upload

---
+++ Week6/data.js
@@ -0,0 +1,64 @@
+const pokemon = [
+  {
+    id: "one",
+    name: "Feraligatr",
+    type: "Water",
+    image:
+      '<a href="https://pokemondb.net/sprites/feraligatr"><img src="https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/feraligatr.png" alt="Feraligatr"></a>',
+  },
+
+  {
+    id: "two",
+    name: "Houndoom",
+    type: "Fire/Dark",
+    image:
+      '<a href="https://pokemondb.net/sprites/houndoom"><img src="https://img.pokemondb.net/sprites/black-white/normal/houndoom.png" alt="Houndoom"></a>',
+  },
+
+  {
+    id: "three",
+    name: "Tyranitar",
+    type: "Dark/Ground",
+    image:
+      '<a href="https://pokemondb.net/sprites/tyranitar"><img src="https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/tyranitar.png" alt="Tyranitar"></a>',
+  },
+
+  {
+    id: "four",
+    name: "Umbreon",
+    type: "Dark",
+    image:
+      '<a href="https://pokemondb.net/sprites/umbreon"><img src="https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/umbreon.png" alt="Umbreon"></a>',
+  },
+
+  {
+    id: "five",
+    name: "Scizor",
+    type: "Bug/Steel",
+    image:
+      '<a href="https://pokemondb.net/sprites/scizor"><img src="https://img.pokemondb.net/sprites/heartgold-soulsilver/normal/scizor.png" alt="Scizor"></a>',
+  },
+
+  {
+    id: "six",
+    name: "Charizard",
+    type: "Fire/Flying",
+    image:
+      '<a href="https://pokemondb.net/sprites/charizard"><img src="https://img.pokemondb.net/sprites/heartgold-soulsilver/shiny/charizard.png" alt="Mew"></a>',
+  },
+];
+
+const trainer = {
+  name: "Yeshua",
+  sprite: "./Sprite.png",
+  badges: [
+    "./badge1.png",
+    "./badge2.png",
+    "./badge3.png",
+    "./badge4.png",
+    ".//badge5.png",
+    "./badge6.png",
+    "./badge7.png",
+    "./badge8.png",
+  ],
+};
---
+++ Week6/work.js
@@ -0,0 +1,60 @@
+
+let greeting = document.getElementById("greeting");
+
+let hour = new Date().getHours();
+
+if (hour < 12) {
+    greeting.textContent = "Good Morning 🌤️";
+} else if (hour < 18) {
+    greeting.textContent = "Good Afternoon ☀️";
+} else {
+    greeting.textContent = "Good Evening 🌙";
+}
+
+
+
+
+let isGreen = false;
+
+document.getElementById("colorBtn").addEventListener("click", function() {
+    if (isGreen === false) {
+        document.body.style.background = "darkgreen";
+        isGreen = true;
+    } else {
+        document.body.style.background = "linear-gradient(to bottom, #b2dcf9, #c9ffb8)";
+        isGreen = false;
+    }
+});
+
+document.getElementById("menuBtn").addEventListener("click", function() {
+    let menu = document.getElementById("menu");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
+
+
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(function(card) {
+    card.addEventListener("click", function() {
+        document.getElementById("message").textContent =
+            "You clicked the " + card.textContent;
+    });
+});
+
+
+
+cards.forEach(function(card) {
+    card.addEventListener("mouseover", function() {
+        card.style.transform = "scale(1.1)";
+    });
+
+    card.addEventListener("mouseout", function() {
+        card.style.transform = "scale(1)";
+    });
+});
